722bf5217ab839853061bd69ea1856fea9fd5bc9
Martha A <mladame43@gmail.com>
2022-09-13 22:15:37 -0700

    added questions to index.js, will edit questions & license section

--- index.js
+++ index.js
@@ -12,8 +12,46 @@ const questions = [
         type: "input",
         name: "title",
         message: "What is the title for this README.md?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "What is the description for this project?"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What is the title for this README.md?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What is the title for this README.md?"
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "What is the title for this README.md?"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "What is the title for this README.md?"
+    },
+    // need github username -> link and email -> contact
+    {
+        type: "input",
+        name: "questions",
+        message: "What is the title for this README.md?"
+    },
+    // make into list optoin
+    {
+        type: "input",
+        name: "license",
+        message: "What is the title for this README.md?"
     }
-    // add sections: description, table of contents, installation, usage, license, contributing, tests, questions
+
+    // add sections: description, installation, usage, license, contributing, tests, questions(enter github username), email,
 ];
 
 // TODO: Create a function to write README file
